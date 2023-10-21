f580e605955554fb0331d61c73642542cac9baa3
jpsoul-dev <jpsoul.dev@gmail.com>
2023-10-20 22:24:42 -0300

    update router
    atualização da rota para o github pages

--- src/router.tsx
+++ src/router.tsx
@@ -7,9 +7,9 @@ import { DefaultLayout } from './components/layouts/Default'
 export function Router() {
   return (
     <Routes>
-      <Route path="/github-blog" element={<DefaultLayout />}>
-        <Route path="/github-blog" element={<BlogPage />} />
-        <Route path="/github-blog/post/:issueNumber" element={<PostPage />} />
+      <Route path="/github_blog" element={<DefaultLayout />}>
+        <Route path="/github_blog" element={<BlogPage />} />
+        <Route path="/github_blog/post/:issueNumber" element={<PostPage />} />
       </Route>
     </Routes>
   )
